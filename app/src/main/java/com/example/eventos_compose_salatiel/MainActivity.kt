bac915a8ad1d2cedfbf91d1ca08ef0fb31663455
SalatielGenol <salatiel@genol.es>
2023-01-25 23:04:18 +0100

    Locuras con Ints

--- app/src/main/java/com/example/eventos_compose_salatiel/MainActivity.kt
+++ app/src/main/java/com/example/eventos_compose_salatiel/MainActivity.kt
@@ -14,17 +14,21 @@ import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.outlined.Delete
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.core.text.isDigitsOnly
 import com.example.eventos_compose_salatiel.ui.theme.EventosComposeSalatielTheme
 
 class MainActivity : ComponentActivity() {
@@ -42,68 +46,89 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen(){
-    val (globalCounter, globalCounterOnChange) = rememberSaveable { mutableStateOf(0) }
+    var globalCounter: Int by rememberSaveable { mutableStateOf(0) }
+
     Column(modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.SpaceEvenly) {
-            ContadorIndividual(
-                numCountName = stringResource(R.string.count_name_button_1),
-                globalCounter = globalCounter,
-                globalCounterOnChange = globalCounterOnChange
-            )
-            ContadorIndividual(
-                numCountName = stringResource(R.string.count_name_button_2),
-                globalCounter = globalCounter,
-                globalCounterOnChange = globalCounterOnChange
-            )
-            ContadorGlobal(
-                globalCounter = globalCounter,
-                globalCounterOnChange = globalCounterOnChange
-            )
+    horizontalAlignment = Alignment.CenterHorizontally,
+    verticalArrangement = Arrangement.SpaceEvenly) {
+        ContadorIndividual(
+            numCountName = stringResource(R.string.count_name_button_1),
+            globalCounter = { globalCounter += it },
+        )
+        ContadorIndividual(
+            numCountName = stringResource(R.string.count_name_button_2),
+            globalCounter = { globalCounter += it },
+        )
+        ContadorGlobal(
+            globalCounter = globalCounter,
+            globalCounterReset = { globalCounter = 0}
+        )
     }
 }
+
+
 
 @Composable
 fun ContadorIndividual(
     numCountName: String,
-    globalCounter: String,
-    globalCounterOnChange: (String) -> Unit,
+    globalCounter: (Int) -> Unit,
 ){
-    val (numInput, numInputOnChange) = rememberSaveable { mutableStateOf("1") }
-    val (numCounter, numCounterOnChange) = rememberSaveable { mutableStateOf("0") }
+    val inputData = 1
+    val clear = 0
+    val focusManager = LocalFocusManager.current
+    val focusRequester = remember { FocusRequester() }
+    var numInputText: String by rememberSaveable { mutableStateOf(inputData.toString()) }
+    var numCounter: Int by rememberSaveable { mutableStateOf(clear) }
+    var numInput: Int by rememberSaveable { mutableStateOf(inputData) }
+    var color: Color by rememberSaveable { mutableStateOf( Color.White) }
+//    var color2: Color by rememberSaveable { mutableStateOf( Colors. }
+
     Column {
+
         Row(verticalAlignment = Alignment.CenterVertically) {
+
             Button(onClick = {
-                numCounterOnChange((numCounter.toInt() + numInput.toInt()).toString())
-                globalCounterOnChange((globalCounter.toInt() + numInput.toInt()).toString())
+                if (numInputText.isBlank()) {
+                    numInputText = numInput.toString()
+                }
+                focusManager.clearFocus()
+                numCounter += numInput
+                globalCounter(numInput)
             }) {
                 Text(text = "$numCountName ($numCounter)")
             }
-            Text(modifier = Modifier.padding(horizontal = 10.dp), text = numCounter)
-            IconoBorrar(modifier = Modifier.clickable { numCounterOnChange("0") })
+            Text(modifier = Modifier.padding(horizontal = 10.dp), text = numCounter.toString())
+            IconoBorrar(onClick = {numCounter = clear})
         }
         Row(verticalAlignment = Alignment.Bottom){
             Text(text = stringResource(R.string.increment_text) + ":")
             BasicTextField(
-                value = numInput,
+                value = numInputText,
                 onValueChange = {
-                    if (it.length > 1) {
-                        numInputOnChange("1")
+                    if(it.isNotBlank() && it.isDigitsOnly()){
+                        numInputText = it
+                        numInput = numInputText.toInt()
                     }else{
-                        numInputOnChange(it)
+                        numInputText = ""
+
                     }
                 },
-                modifier = Modifier.onFocusChanged { focusState ->
-                    when {
-                        focusState.hasFocus -> numInputOnChange("")
-                    } },
+                modifier = Modifier
+                    .onFocusChanged { focusState ->
+                        when {
+                            focusState.hasFocus -> {
+                                numInputText = ""
+                            }
+                        }
+                    }
+                    .focusRequester(focusRequester),
                 keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
                 singleLine = true,
                 textStyle = TextStyle(
                     textAlign = TextAlign.End,
                     color = MaterialTheme.colors.onSurface
                 ),
-                decorationBox = {
+                decorationBox = { innerTextField ->
                     Row(
                         modifier = Modifier
                             .padding(start = 5.dp)
@@ -118,7 +143,7 @@ fun ContadorIndividual(
                         horizontalArrangement = Arrangement.End,
                         verticalAlignment = Alignment.CenterVertically
                     ) {
-                        it()
+                        innerTextField()
                     }
                 }
             )
@@ -127,21 +152,21 @@ fun ContadorIndividual(
 }
 
 @Composable
-fun ContadorGlobal(globalCounter: String, globalCounterOnChange: (String) -> Unit){
+fun ContadorGlobal(globalCounter: Int, globalCounterReset: () -> Unit){
     Column {
         Row(verticalAlignment = Alignment.CenterVertically) {
             Text(text = stringResource(R.string.global_text) + ":")
-            Text(modifier = Modifier.padding(horizontal = 10.dp), text = globalCounter)
-            IconoBorrar(modifier = Modifier.clickable { globalCounterOnChange("0") })
+            Text(modifier = Modifier.padding(horizontal = 10.dp), text = globalCounter.toString())
+            IconoBorrar(onClick = globalCounterReset)
         }
     }
 }
 
 @Composable
-fun IconoBorrar(modifier: Modifier = Modifier){
+fun IconoBorrar(onClick: () -> Unit){
     if (isSystemInDarkTheme()){
-        Icon(Icons.Filled.Delete, contentDescription = "null", modifier = modifier)
+        Icon(Icons.Filled.Delete, contentDescription = "null", modifier = Modifier.clickable { onClick() })
     }else{
-        Icon(Icons.Outlined.Delete, contentDescription = "null", modifier = modifier)
+        Icon(Icons.Outlined.Delete, contentDescription = "null", modifier = Modifier.clickable { onClick() })
     }
 }
